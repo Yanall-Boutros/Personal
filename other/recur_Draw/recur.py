2a185d41afe3aab4fce79d4d86bfc5aa121306c2
Yanall-Boutros <yboutros@ucsc.edu>
2018-11-04 19:22:25 -0800

    restructured file base, removed duplicate rhythms

---
+++ other/recur_Draw/recur.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+# Linear transformation functions
+# f1 is chosen 1 percent of the time
+def f1(x, y):
+   return (0, 0.16*y)
+# f2 is chosen 85% of the time
+def f2(x_n, y_n):
+   return ((0.85*x_n + 0.04+y_n), (-0.04*x_n + 0.85*y_n + 1.6))
+# f3 is chosen 7% of the time
+def f3(x, y):
+   return ((0.2*x - 0.26*y), (0.23*x+0.22*y+1.6))
+# f4 is chosen 7% of the time
+def f4(x, y):
+   return ((-0.15*x + 0.28*y), (0.26*x + 0.24*y + 0.44))
+
+coors = list()
+# Base Case, x_0 = 0, y_0 = 0
+coors.append((0, 0))
+# Produce Random Data
+np.random.seed(19680801)
+prob_data = np.random.rand(10000)
+i = 0
+for rand_number in prob_data:
+   if int(100*rand_number) > 14:
+      coors.append(f2(coors[i][0], coors[i][1]))
+      i += 1
+   if int(100*rand_number) > 92:
+      coors.append(f3(coors[i][0], coors[i][1]))
+      i += 1
+   if int(100*rand_number) < 7:
+      coors.append(f4(coors[i][0], coors[i][1]))
+      i += 1
+   if int(100*rand_number) > 98:
+      coors.append(f1(coors[i][0], coors[i][1]))
+      i += 1
+x_data = list()
+y_data = list()
+for tup in coors:
+   x_data.append(tup[0])
+   y_data.append(tup[1])
+x_data = np.array(x_data)
+y_data = np.array(y_data)
+plt.plot(x_data, y_data, marker=".",markersize=1, linestyle="None")
+plt.axis('off')
+plt.savefig("Test.pdf")
---
+++ other/violin_runs/runs.py
@@ -0,0 +1,58 @@
+# -----------------------------------------------------------------------
+# Import Statements
+# -----------------------------------------------------------------------
+import numpy as np
+# -----------------------------------------------------------------------
+# Function Definitions
+# -----------------------------------------------------------------------
+np.random.seed(19680801)
+# Future update, use inverse squared probabilities for less fucky results
+prob_percent = 0.87
+even_beat_prob = 0.75
+no_beat_prob = 0.3
+n_runs = 100
+def n_beats(n):
+   # returns an array of length n initialized to zeros
+   measure = []
+   for i in range(n):
+      measure.append(0)
+   return measure
+def drop_the_beat():
+   # Returns either a list with 0 initialized values or a 1, or a 0
+   if np.random.rand() <  prob_percent:
+      if np.random.rand() < no_beat_prob:
+         return 0
+      else:
+         return 1
+   # return a list to be some percent an even integer, and some percent
+   # an odd integer
+   else:
+      subbeat = []
+      if np.random.rand() < even_beat_prob:
+         # generate a even number
+         n = int(-1*np.log(np.random.rand())) + 1
+         n *= 2
+      else:
+         # generate an odd number
+         n = int(-1*np.log(np.random.rand())) + 1
+         n = 2*n - 1
+      for b in range(n):
+         subbeat.append(0)
+         # Recursively call until we have just ones
+         for beat in range(len(subbeat)):
+            subbeat[beat] = drop_the_beat()
+      return subbeat
+# let the length of an array denote the maximum number of beats to fit in
+# a measure. with 0 indicating a break
+# -----------------------------------------------------------------------
+# Main Function
+# -----------------------------------------------------------------------
+# inside every beat, is either a 1, indicating a quarter note, or another
+# list, indicating the length of that list is the number of subdivisions
+# of the beat
+for j in [2, 4, 6, 8, 10]: 
+   for i in range(n_runs):
+      a = n_beats(j)
+      for i in range(len(a)):
+         a[i] = drop_the_beat()
+      print(a)
